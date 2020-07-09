9f79bcc7fa3c56e0eb9c193a85c5c702032716b1
Siwen19 <59066429+Siwen19@users.noreply.github.com>
2020-07-09 21:20:22 +0800

    Update Header.jsx

--- react-小书/theme-switch/src/Header.jsx
+++ react-小书/theme-switch/src/Header.jsx
@@ -1,35 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-class Header extends Component {  
-    static contextTypes = {
-        store: PropTypes.object
+class Header extends Component {
+    static propTypes = {
+        themeColor: PropTypes.string
     }
-    constructor () {
-        super();
-        this.state = { themeColor: ''}
-    }
-    componentWillMount () {
-        this._updateThemeColor();
-    }
-    _updateThemeColor () {
-        const { store } = this.context;
-        const state = store.getState();
-        this.setState({ themeColor: state.themeColor })
-    }
-    // 在数据变化的时候
-    componentWillMount () {
-        const { store } = this.context;
-        // 获取最新的 themeColor 
-        this._updateThemeColor();
-        // 重新渲染组件
-        store.subscribe(() => this._updateThemeColor());
-    }
-    render() { 
-        return ( 
-            <h1 style={{ color: this.state.themeColor }}>React.js 小书</h1>
-         );
+    render() {
+        return (
+            <h1 style={{ color: this.props.themeColor }}>React.js 小书</h1>
+        );
     }
 }
- 
+
+const mapStateToProps = (state) => {
+    return {
+        themeColor: state.themeColor
+    }
+}
+
+Header = connect(mapStateToProps)(Header);
+
 export default Header;
